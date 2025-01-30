047bf454391456d44de420ac83f1a01d68fa1b7f
Gisele <giselesouza001@outlook.com>
2025-01-30 13:07:03 -0300

    Adicionando desenvolvimento da função exibirNomes e modificar função adicionarAmigo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ Visualizar a lista: Os nomes inseridos aparecerão em uma lista abaixo do campo 
 
 Sorteio aleatório: Ao clicar no botão "Sortear Amigo", um nome da lista será selecionado aleatoriamente e exibido na página.*/
 
-let listaNomes = [];
+let amigos = [];
 
 function adicionarAmigo() {
     let nome = document.getElementById("amigo").value;
@@ -16,17 +16,24 @@ function adicionarAmigo() {
     if(nome == "") {
         alert("Por favor, insira um nome válido!");
     } else {
-        listaNomes.push(nome);
-
-        document.getElementById("amigo").value = "";
+        amigos.push(nome);
 
         exibirNomes();
+
+        document.getElementById("amigo").value = "";
     }
 }
 
 function exibirNomes() {
-    
+    let listaAmigos = document.getElementById("listaAmigos");
 
+    listaAmigos.innerHTML = "";
+
+    amigos.forEach(function(nome){
+        let li = document.createElement("li");
+        li.textContent = nome;
+        listaAmigos.appendChild(li);
+    });
 }
 
 
