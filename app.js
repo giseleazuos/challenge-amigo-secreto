f72b65a8ae4c1803065a399a14cdc61b7de0d0f6
Gisele <giselesouza001@outlook.com>
2025-01-30 21:10:13 -0300

    Salvando ajustes finais: excluindo linha comentada e formatando o código

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-
-/*Adicionar nomes: Os usuários escreverão o nome de um amigo em um campo de texto e o adicionarão a uma lista visível ao clicar em "Adicionar".
-
-Validar entrada: Se o campo de texto estiver vazio, o programa exibirá um alerta solicitando um nome válido.
-
-Visualizar a lista: Os nomes inseridos aparecerão em uma lista abaixo do campo de entrada.
-
-Sorteio aleatório: Ao clicar no botão "Sortear Amigo", um nome da lista será selecionado aleatoriamente e exibido na página.*/
-
 let amigos = [];
 
 function adicionarAmigo() {
     let nome = document.getElementById("amigo").value;
 
-    if(nome.trim() === "") {
+    if (nome.trim() === "") {
         alert("Por favor, insira um nome válido!");
     } else {
         amigos.push(nome);
@@ -29,7 +19,7 @@ function exibirNomes() {
 
     listaAmigos.innerHTML = "";
 
-    amigos.forEach(function(nome){
+    amigos.forEach(function (nome) {
         let li = document.createElement("li");
         li.textContent = nome;
         listaAmigos.appendChild(li);
@@ -38,11 +28,11 @@ function exibirNomes() {
 
 function sortearAmigo() {
 
-    if(amigos.length != 0) {
+    if (amigos.length != 0) {
         let indiceAleatorio = Math.floor(Math.random() * amigos.length);
 
         let nomeSorteado = amigos[indiceAleatorio];
         document.getElementById("resultado").textContent = `O amigo secreto sorteado é: ${nomeSorteado}`;
-    } 
+    }
 }
 
